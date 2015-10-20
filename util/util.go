6d864f5e670bdbf93db87b0797fa73f456c71e29
Ryan Ernst <rernst76@gmail.com>
2015-10-19 22:40:11 -0400

    Changed transItem to use value struct
    Necessary to capture when a value was not set prior to the
    transaction. Can't set an int to nil, so have to use a struct
    instead.

--- util/util.go
+++ util/util.go
@@ -1,14 +1,35 @@
 package util
 
-// Struct store all destructive modifications of a transaction
+// Since we can't set an int to nil, need to make a type which allows
+// us to say, before x happened "name" was set to nil (meaning it didn't exist)
+type value struct {
+	val   int
+	isNil bool
+}
+
+// Struct to store all destructive modifications of a transaction
 type transItem struct {
-	trans map[string]int
+	trans map[string]value
 	next  *transItem
 }
 
 // Linked list stack implementation to store transItems
 type TransStack struct {
-	head  *TransactionItem
-	tail  *TransactionItem
-	count int
+	head *transItem
+	tail *transItem
+	cnt  int
+}
+
+// Makes new Transaction Stack and return pointer
+func New() *TransStack {
+	var newTS TransStack
+	newTS.head = nil
+	newTS.tail = nil
+	newTS.cnt = 0
+	return &newTS
+}
+
+// When doing a destructive modification, use StoreVal to save value of
+func (t *TransStack) StoreVal() {
+
 }
