e789998fa038d4e65e78641619c88267c91d994e
Ryan Ernst <rernst76@gmail.com>
2015-10-19 21:22:28 -0400

    Started implementation of transaction stack

--- util/util.go
+++ util/util.go
@@ -1,1 +1,14 @@
 package util
+
+// Struct store all destructive modifications of a transaction
+type transItem struct {
+	trans map[string]int
+	next  *transItem
+}
+
+// Linked list stack implementation to store transItems
+type TransStack struct {
+	head  *TransactionItem
+	tail  *TransactionItem
+	count int
+}
